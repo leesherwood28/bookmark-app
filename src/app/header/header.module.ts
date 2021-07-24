4672c928fd9068bd7adf6406eeb0147a581c56e2
lee sherwood <leesherwood28@googlemail.com>
2021-07-24 12:15:33 +0100

    Initial add of theme switch component

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { ThemeSwitchComponent } from './theme-switch/theme-switch.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    ThemeSwitchComponent
   ],
   imports: [
     CommonModule
---
+++ src/app/header/theme-switch/theme-switch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-switch',
+  templateUrl: './theme-switch.component.html',
+  styleUrls: ['./theme-switch.component.scss']
+})
+export class ThemeSwitchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
