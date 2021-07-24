4e363b52f1eeac7c2d1c4a1622cc8e7f92eaf7d9
lee sherwood <leesherwood28@googlemail.com>
2021-07-24 14:19:35 +0100

    Setting up theme toggle

--- src/app/shared/theme-toggle/theme-toggle.module.ts
+++ src/app/shared/theme-toggle/theme-toggle.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ThemeToggleComponent } from './theme-toggle.component';
 
 @NgModule({
   declarations: [ThemeToggleComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatSlideToggleModule],
   exports: [ThemeToggleComponent],
 })
 export class ThemeToggleModule {}
