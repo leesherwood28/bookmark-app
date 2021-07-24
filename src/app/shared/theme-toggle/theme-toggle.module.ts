f0e5f7d10054069edd4ef4bf725bf86941699700
lee sherwood <leesherwood28@googlemail.com>
2021-07-24 14:35:34 +0100

    Adding until destory dependency

--- src/app/shared/theme-toggle/theme-toggle.module.ts
+++ src/app/shared/theme-toggle/theme-toggle.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ThemeToggleComponent } from './theme-toggle.component';
 
 @NgModule({
   declarations: [ThemeToggleComponent],
-  imports: [CommonModule, MatSlideToggleModule],
+  imports: [CommonModule, MatSlideToggleModule, ReactiveFormsModule],
   exports: [ThemeToggleComponent],
 })
 export class ThemeToggleModule {}
