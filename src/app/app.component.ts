c43969a6d524426ed088c5eb7309d484e961c3a6
lee sherwood <leesherwood28@googlemail.com>
2021-07-24 16:08:08 +0100

    Nested home content and ensured theme class applied

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ThemeService } from './shared/theme/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +12,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 /**
  * The main app component
  */
-export class AppComponent {}
+export class AppComponent {
+  readonly isLightTheme$: Observable<boolean>;
+
+  constructor(private themeService: ThemeService) {
+    this.isLightTheme$ = this.themeService
+      .selectTheme()
+      .pipe(map((theme) => theme === 'light'));
+  }
+}
