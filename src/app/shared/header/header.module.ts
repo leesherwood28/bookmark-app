2bbc227d239f5033445e600544603cf8b6a18431
lee sherwood <leesherwood28@googlemail.com>
2021-07-24 15:16:04 +0100

    Adding icon to bookmark manager title

--- src/app/shared/header/header.module.ts
+++ src/app/shared/header/header.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 import { HeaderComponent } from './header.component';
 import { ThemeToggleModule } from '../theme-toggle/theme-toggle.module';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, ThemeToggleModule],
+  imports: [CommonModule, ThemeToggleModule, MatIconModule],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
