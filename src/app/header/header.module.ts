7e4894b48029599349f8a5581728eb9b08bb9749
lee sherwood <leesherwood28@googlemail.com>
2021-07-24 12:19:23 +0100

    Adding header to app component

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -3,15 +3,9 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { ThemeSwitchComponent } from './theme-switch/theme-switch.component';
 
-
-
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    ThemeSwitchComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HeaderComponent, ThemeSwitchComponent],
+  imports: [CommonModule],
+  exports: [HeaderComponent],
 })
-export class HeaderModule { }
+export class HeaderModule {}
