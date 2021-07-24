c4606d7d0783b6d3a59484e765ba0464540e5229
lee sherwood <leesherwood28@googlemail.com>
2021-07-24 12:30:53 +0100

    Setting components to on push change detection

--- src/app/header/theme-switch/theme-switch.component.ts
+++ src/app/header/theme-switch/theme-switch.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-theme-switch',
   templateUrl: './theme-switch.component.html',
-  styleUrls: ['./theme-switch.component.scss']
+  styleUrls: ['./theme-switch.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ThemeSwitchComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
