1da3325d414e31bb39f890702852703489a56184
lee sherwood <leesherwood28@googlemail.com>
2021-07-24 13:29:08 +0100

    removal of old files

---
+++ src/app/shared/theme-toggle/theme-toggle.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ThemeToggleComponent } from './theme-toggle.component';
+
+@NgModule({
+  declarations: [ThemeToggleComponent],
+  imports: [CommonModule],
+  exports: [ThemeToggleComponent],
+})
+export class ThemeToggleModule {}
