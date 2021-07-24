6028f95891817df8b3a7a6ddabe199bd042a7a77
lee sherwood <leesherwood28@googlemail.com>
2021-07-24 13:36:40 +0100

    Improving header styles

--- src/app/shared/header/header.module.ts
+++ src/app/shared/header/header.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
+import { ThemeToggleModule } from '../theme-toggle/theme-toggle.module';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ThemeToggleModule],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
