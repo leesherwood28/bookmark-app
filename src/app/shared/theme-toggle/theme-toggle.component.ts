bf7e7814137bc0840eaec48ddedb92f396843879
lee sherwood <leesherwood28@googlemail.com>
2021-07-24 15:06:44 +0100

    Ensured theme toggle state is functional

--- src/app/shared/theme-toggle/theme-toggle.component.ts
+++ src/app/shared/theme-toggle/theme-toggle.component.ts
@@ -1,13 +1,51 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { ThemeService } from '../theme/theme.service';
 
+@UntilDestroy()
 @Component({
   selector: 'app-theme-toggle',
   templateUrl: './theme-toggle.component.html',
   styleUrls: ['./theme-toggle.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
+/**
+ * Component is used to toggle between light and dark theme
+ * for the application
+ */
 export class ThemeToggleComponent implements OnInit {
-  constructor() {}
+  readonly themeControl = new FormControl();
 
-  ngOnInit(): void {}
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.setupThemeChangeSub();
+    this.setupThemeUpdateSub();
+  }
+
+  /**
+   * Sets up a subcription that ensures theme changes
+   * are reflected in the state of the toggle control
+   */
+  private setupThemeChangeSub() {
+    this.themeControl.valueChanges
+      .pipe(untilDestroyed(this))
+      .subscribe((isDarkTheme: boolean) => {
+        this.themeService.setTheme(isDarkTheme ? 'dark' : 'light');
+      });
+  }
+
+  /**
+   * Sets up a subscription that ensure toggles to the
+   * theme update the global theme state
+   */
+  private setupThemeUpdateSub() {
+    this.themeService
+      .selectTheme()
+      .pipe(untilDestroyed(this))
+      .subscribe((theme) =>
+        this.themeControl.setValue(theme === 'dark', { emitEvent: false })
+      );
+  }
 }
