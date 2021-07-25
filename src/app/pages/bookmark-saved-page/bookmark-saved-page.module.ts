72248e78a278a94fc1507c1c39b21baf4955d8f5
lee sherwood <leesherwood28@googlemail.com>
2021-07-25 21:07:50 +0100

    Migrating bookmark components

--- src/app/pages/bookmark-saved-page/bookmark-saved-page.module.ts
+++ src/app/pages/bookmark-saved-page/bookmark-saved-page.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { BookmarkOverviewModule } from 'src/app/features/bookmark-overview/bookmark-overview.module';
+import { BookmarkOverviewModule } from 'src/app/bookmark/bookmark-overview/bookmark-overview.module';
 import { BookmarkSavedPageRoutingModule } from './bookmark-saved-page-routing.module';
 import { BookmarkSavedPageComponent } from './bookmark-saved-page.component';
 
--- src/app/pages/overview-page/overview-page.module.ts
+++ src/app/pages/overview-page/overview-page.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { BookmarkEditorModule } from 'src/app/features/bookmark-editor/bookmark-editor.module';
-import { BookmarkListModule } from 'src/app/features/bookmark-list/bookmark-list.module';
+import { BookmarkEditorModule } from 'src/app/bookmark/bookmark-editor/bookmark-editor.module';
+import { BookmarkListModule } from 'src/app/bookmark/bookmark-list/bookmark-list.module';
 import { OverviewPageRoutingModule } from './overview-page-routing.module';
 import { OverviewPageComponent } from './overview-page.component';
 
