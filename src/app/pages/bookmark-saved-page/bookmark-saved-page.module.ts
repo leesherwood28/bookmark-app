a7696e87fe005c16abccf9659c97fc826072f0b1
lee sherwood <leesherwood28@googlemail.com>
2021-07-25 18:11:03 +0100

    Added back functionality to bookmark save screen

--- src/app/pages/bookmark-saved-page/bookmark-saved-page.module.ts
+++ src/app/pages/bookmark-saved-page/bookmark-saved-page.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { BookmarkOverviewModule } from 'src/app/features/bookmark-overview/bookmark-overview.module';
 import { BookmarkSavedPageRoutingModule } from './bookmark-saved-page-routing.module';
@@ -12,6 +13,7 @@ import { BookmarkSavedPageComponent } from './bookmark-saved-page.component';
     BookmarkOverviewModule,
     BookmarkSavedPageRoutingModule,
     MatCardModule,
+    MatButtonModule,
   ],
 })
 export class BookmarkSavedPageModule {}
