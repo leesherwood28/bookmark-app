39963373c62f4281b22bb830ac5a436837d731da
lee sherwood <leesherwood28@googlemail.com>
2021-07-25 18:08:56 +0100

    Improving display of bookmark saved display

--- src/app/pages/bookmark-saved-page/bookmark-saved-page.module.ts
+++ src/app/pages/bookmark-saved-page/bookmark-saved-page.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { BookmarkOverviewModule } from 'src/app/features/bookmark-overview/bookmark-overview.module';
-import { BookmarkSavedPageComponent } from './bookmark-saved-page.component';
 import { BookmarkSavedPageRoutingModule } from './bookmark-saved-page-routing.module';
+import { BookmarkSavedPageComponent } from './bookmark-saved-page.component';
 
 @NgModule({
   declarations: [BookmarkSavedPageComponent],
-  imports: [CommonModule, BookmarkOverviewModule, BookmarkSavedPageRoutingModule],
+  imports: [
+    CommonModule,
+    BookmarkOverviewModule,
+    BookmarkSavedPageRoutingModule,
+    MatCardModule,
+  ],
 })
 export class BookmarkSavedPageModule {}
