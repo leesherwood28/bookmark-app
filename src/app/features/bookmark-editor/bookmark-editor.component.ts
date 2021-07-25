a181e1fbb2c4cf52ef63b46af3d1df384f164c0b
lee sherwood <leesherwood28@googlemail.com>
2021-07-25 12:31:57 +0100

    Adding basic controls to bookmark editor

--- src/app/features/bookmark-editor/bookmark-editor.component.ts
+++ src/app/features/bookmark-editor/bookmark-editor.component.ts
@@ -1,16 +1,30 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { BookmarkService } from 'src/app/core/bookmark/bookmark.service';
 
 @Component({
   selector: 'app-bookmark-editor',
   templateUrl: './bookmark-editor.component.html',
   styleUrls: ['./bookmark-editor.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BookmarkEditorComponent implements OnInit {
+  bookmarkForm!: FormGroup;
+  nameControl!: FormControl;
+  urlControl!: FormControl;
 
-  constructor() { }
+  constructor(private bookmarkService: BookmarkService) {}
 
   ngOnInit(): void {
+    this.setupForm();
   }
 
+  private setupForm() {
+    this.nameControl = new FormControl();
+    this.urlControl = new FormControl();
+    this.bookmarkForm = new FormGroup({
+      name: this.nameControl,
+      url: this.urlControl,
+    });
+  }
 }
--- src/app/features/bookmark-editor/bookmark-editor.module.ts
+++ src/app/features/bookmark-editor/bookmark-editor.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BookmarkEditorComponent } from './bookmark-editor.component';
 
 @NgModule({
   declarations: [BookmarkEditorComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    ReactiveFormsModule,
+  ],
   exports: [BookmarkEditorComponent],
 })
 export class BookmarkEditorModule {}
