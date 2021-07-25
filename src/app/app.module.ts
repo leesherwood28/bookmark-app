de1cb40fc710b986adb5447a3dfc28211296ef27
lee sherwood <leesherwood28@googlemail.com>
2021-07-25 10:30:13 +0100

    Addition of overview component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OverviewModule } from './pages/overview/overview.module';
 import { HeaderModule } from './shared/header/header.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HeaderModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HeaderModule,
+    OverviewModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/overview/overview.module.ts
+++ src/app/pages/overview/overview.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { OverviewRoutingModule } from './overview-routing.module';
-
+import { OverviewComponent } from './overview.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    OverviewRoutingModule
-  ]
+  declarations: [OverviewComponent],
+  imports: [CommonModule],
 })
-export class OverviewModule { }
+export class OverviewModule {}
