e708c6b164cd2029e60040930d0a7863e14af36d
lee sherwood <leesherwood28@googlemail.com>
2021-07-25 17:17:51 +0100

    Adding back in missing routing module

--- src/app/pages/bookmark-saved-page/bookmark-saved-page.module.ts
+++ src/app/pages/bookmark-saved-page/bookmark-saved-page.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookmarkOverviewModule } from 'src/app/features/bookmark-overview/bookmark-overview.module';
 import { BookmarkSavedPageComponent } from './bookmark-saved-page.component';
+import { BookmarkSavedPageRoutingModule } from './bookmark-saved-page-routing.module';
 
 @NgModule({
   declarations: [BookmarkSavedPageComponent],
-  imports: [CommonModule, BookmarkOverviewModule],
+  imports: [CommonModule, BookmarkOverviewModule, BookmarkSavedPageRoutingModule],
 })
 export class BookmarkSavedPageModule {}
