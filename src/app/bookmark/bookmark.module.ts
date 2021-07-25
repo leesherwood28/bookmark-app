4caa50eab1a5a34e9277ed19935bc842ea38d653
lee sherwood <leesherwood28@googlemail.com>
2021-07-25 21:20:54 +0100

    Fixing up imports

--- src/app/bookmark/bookmark.module.ts
+++ src/app/bookmark/bookmark.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BookmarkRoutingModule } from './bookmark-routing.module';
 import { BookmarkEditorComponent } from './bookmark-editor/bookmark-editor.component';
 import { BookmarkListComponent } from './bookmark-list/bookmark-list.component';
@@ -17,6 +20,14 @@ import { BookmarkSavedPageComponent } from './bookmark-saved-page/bookmark-saved
     BookmarkListComponent,
     BookmarkSavedPageComponent,
   ],
-  imports: [CommonModule, BookmarkRoutingModule],
+  imports: [
+    CommonModule,
+    BookmarkRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class BookmarkModule {}
