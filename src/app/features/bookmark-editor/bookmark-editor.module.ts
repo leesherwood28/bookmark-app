b2a284cfa3179234d3993cc84a9db45e7d247ae9
lee sherwood <leesherwood28@googlemail.com>
2021-07-25 10:52:04 +0100

    Initial addition of bookmark editor

---
+++ src/app/features/bookmark-editor/bookmark-editor.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BookmarkEditorComponent } from './bookmark-editor.component';
+
+
+
+@NgModule({
+  declarations: [
+    BookmarkEditorComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BookmarkEditorModule { }
