33420fa715d7da5a332159390267f90de582ddf3
lee sherwood <leesherwood28@googlemail.com>
2021-07-25 18:02:27 +0100

    Made header title a link back to the home page

--- src/app/shared/header/header.module.ts
+++ src/app/shared/header/header.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header.component';
 import { ThemeToggleModule } from '../theme-toggle/theme-toggle.module';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, ThemeToggleModule, MatIconModule, MatToolbarModule],
+  imports: [
+    CommonModule,
+    ThemeToggleModule,
+    MatIconModule,
+    MatToolbarModule,
+    RouterModule,
+  ],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
