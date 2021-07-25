91166fee048c5a7e40e12319803f8df5eb0c2523
lee sherwood <leesherwood28@googlemail.com>
2021-07-25 12:49:57 +0100

    Adding button module to bookmark editor

--- src/app/features/bookmark-editor/bookmark-editor.module.ts
+++ src/app/features/bookmark-editor/bookmark-editor.module.ts
@@ -4,6 +4,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { BookmarkEditorComponent } from './bookmark-editor.component';
 
 @NgModule({
@@ -12,7 +14,9 @@ import { BookmarkEditorComponent } from './bookmark-editor.component';
     CommonModule,
     MatFormFieldModule,
     MatInputModule,
+    MatCardModule,
     MatIconModule,
+    MatButtonModule,
     ReactiveFormsModule,
   ],
   exports: [BookmarkEditorComponent],
