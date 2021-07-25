2d60310b41acaa5983705fab8e368051baf1a335
lee sherwood <leesherwood28@googlemail.com>
2021-07-25 17:16:46 +0100

    Refactoring bookmark saved page to retreive bookmark

--- src/app/pages/bookmark-saved-page/bookmark-saved-page.component.ts
+++ src/app/pages/bookmark-saved-page/bookmark-saved-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Bookmark } from 'src/app/core/bookmark/bookmark';
 
 @Component({
   selector: 'app-bookmark-saved-page',
@@ -7,7 +10,11 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BookmarkSavedPageComponent implements OnInit {
-  constructor() {}
+  savedBookmark!: Observable<Bookmark>;
 
-  ngOnInit(): void {}
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    console.log(this.route);
+  }
 }
