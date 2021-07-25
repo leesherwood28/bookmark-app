e1f356374fe5f0cc64603881053739507a7fac01
lee sherwood <leesherwood28@googlemail.com>
2021-07-25 11:25:49 +0100

    Refactoring header to use material toolbar

--- src/app/shared/header/header.module.ts
+++ src/app/shared/header/header.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { HeaderComponent } from './header.component';
 import { ThemeToggleModule } from '../theme-toggle/theme-toggle.module';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, ThemeToggleModule, MatIconModule],
+  imports: [CommonModule, ThemeToggleModule, MatIconModule, MatToolbarModule],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
