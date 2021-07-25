de85f9955048ec2c83ebcfcde61a98675b168ba9
lee sherwood <leesherwood28@googlemail.com>
2021-07-25 17:08:23 +0100

    Made bookmark overview a dumb component

--- src/app/features/bookmark-overview/bookmark-overview.component.ts
+++ src/app/features/bookmark-overview/bookmark-overview.component.ts
@@ -1,8 +1,5 @@
 import { ChangeDetectionStrategy, Component, Input, OnChanges } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Bookmark } from 'src/app/core/bookmark/bookmark';
-import { BookmarkService } from 'src/app/core/bookmark/bookmark.service';
-import { isNil } from 'src/app/util/is-nil.fn';
 
 @Component({
   selector: 'app-bookmark-overview',
@@ -11,16 +8,5 @@ import { isNil } from 'src/app/util/is-nil.fn';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BookmarkOverviewComponent implements OnChanges {
-  @Input() bookmarkId!: string;
-
-  bookmark$!: Observable<Bookmark | undefined>;
-
-  constructor(private bookmarkService: BookmarkService) {}
-
-  ngOnChanges(): void {
-    if (isNil(this.bookmarkId)) {
-      throw new Error('No bookmark id provided to control');
-    }
-    this.bookmark$ = this.bookmarkService.selectBookmark(this.bookmarkId);
-  }
+  @Input() bookmark!: Bookmark;
 }
