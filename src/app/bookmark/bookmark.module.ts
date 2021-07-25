b15696bdcca7006736ae8eab326aee8e0fe3c17c
lee sherwood <leesherwood28@googlemail.com>
2021-07-25 21:18:34 +0100

    removing old modules

--- src/app/bookmark/bookmark.module.ts
+++ src/app/bookmark/bookmark.module.ts
@@ -2,9 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BookmarkRoutingModule } from './bookmark-routing.module';
+import { BookmarkEditorComponent } from './bookmark-editor/bookmark-editor.component';
+import { BookmarkListComponent } from './bookmark-list/bookmark-list.component';
+import { BookmarkOverviewPageComponent } from './bookmark-overview-page/bookmark-overview-page.component';
+import { BookmarkOverviewComponent } from './bookmark-overview/bookmark-overview.component';
+import { BookmarkSavedPageComponent } from './bookmark-saved-page/bookmark-saved-page.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BookmarkEditorComponent,
+    BookmarkListComponent,
+    BookmarkOverviewPageComponent,
+    BookmarkOverviewComponent,
+    BookmarkListComponent,
+    BookmarkSavedPageComponent,
+  ],
   imports: [CommonModule, BookmarkRoutingModule],
 })
 export class BookmarkModule {}
