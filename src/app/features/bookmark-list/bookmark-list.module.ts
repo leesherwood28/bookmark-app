e1f873980982e6c71702976ac586d4ffbad211ce
lee sherwood <leesherwood28@googlemail.com>
2021-07-25 14:18:07 +0100

    Adding bookmark list module

--- src/app/features/bookmark-list/bookmark-list.module.ts
+++ src/app/features/bookmark-list/bookmark-list.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { BookmarkListComponent } from './bookmark-list.component';
 
 @NgModule({
   declarations: [BookmarkListComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   exports: [BookmarkListComponent],
 })
 export class BookmarkListModule {}
