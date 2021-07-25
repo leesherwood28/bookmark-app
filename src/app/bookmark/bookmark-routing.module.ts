8243592befae97a644678dd376cabbd8ac6b1a9d
lee sherwood <leesherwood28@googlemail.com>
2021-07-25 21:37:02 +0100

    adding back in save success gaurd

--- src/app/bookmark/bookmark-routing.module.ts
+++ src/app/bookmark/bookmark-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookmarkOverviewComponent } from './bookmark-overview/bookmark-overview.component';
 import { BookmarkSavedPageComponent } from './bookmark-saved-page/bookmark-saved-page.component';
+import { BookmarkSavedPageGuard } from './bookmark-saved-page/bookmark-saved-page.guard';
 
 const routes: Routes = [
   {
     path: 'success',
     component: BookmarkSavedPageComponent,
+    canActivate: [BookmarkSavedPageGuard],
   },
   {
     path: '',
