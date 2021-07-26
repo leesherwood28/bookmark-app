b5ed267e98daaf1deba7588294ca6febf891c666
lee sherwood <leesherwood28@googlemail.com>
2021-07-26 22:54:26 +0100

    Making store work with local storage

--- src/app/core/store/store.ts
+++ src/app/core/store/store.ts
@@ -1,14 +1,21 @@
 import { BehaviorSubject, Observable } from 'rxjs';
+import { isNil } from '../util/is-nil.fn';
+import { PersistStorageProviderService } from './persist-storage-provider.service';
 
-export abstract class Store<T> {
-  private readonly storedState$: BehaviorSubject<T>;
+export class Store<T> {
+  private storedState$!: BehaviorSubject<T>;
 
-  constructor(private name: string) {
-    this.initStore();
+  constructor(
+    private key: string,
+    private initState: T,
+    private persistStore?: PersistStorageProviderService
+  ) {
+    this.initializeStore();
   }
 
-  private initStore() {
-    this.storedState$ = new BehaviorSubject<T>();
+  private initializeStore() {
+    const initialState = this.getInitialState();
+    this.storedState$ = new BehaviorSubject<T>(initialState);
   }
 
   protected update(data: Readonly<T>) {}
@@ -19,5 +26,29 @@ export abstract class Store<T> {
 
   protected selectData(): Observable<T> {
     return this.storedState$.asObservable();
+  }
+
+  /**
+   * Gets the initial data
+   * @return {T} The initial state
+   */
+  private getInitialState(): T {
+    if (!this.canUsePersistence()) {
+      return this.initState;
+    }
+
+    const persistedState = this.persistStore?.getPersistedData(this.key) as T | null;
+    if (!isNil(persistedState)) {
+      return persistedState;
+    }
+    return this.initState;
+  }
+
+  /**
+   * Determines if persistence can be used
+   * @return {boolean} value indicating if persistence can be used
+   */
+  private canUsePersistence(): boolean {
+    return !isNil(this.persistStore) && this.persistStore.storageAvailable();
   }
 }
