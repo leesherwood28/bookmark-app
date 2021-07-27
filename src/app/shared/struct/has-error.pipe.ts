2712b864cdd4fee78460420d7160024d3f76b9ff
lee sherwood <leesherwood28@googlemail.com>
2021-07-27 22:45:11 +0100

    Fixed has Error pipe to not make it pure

--- src/app/shared/struct/has-error.pipe.ts
+++ src/app/shared/struct/has-error.pipe.ts
@@ -3,9 +3,10 @@ import { FormControl } from '@angular/forms';
 
 @Pipe({
   name: 'hasError',
+  pure: false,
 })
 export class HasErrorPipe implements PipeTransform {
-  transform(control: FormControl, error: string): unknown {
+  transform(control: FormControl, error: string): any {
     return control?.errors?.[error];
   }
 }
